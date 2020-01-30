c2113b2cfe2690192510c03c685c5adf47754889
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-30 17:41:01 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -39,10 +39,11 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, pledges.amount
-  FROM projects
-  JOIN pledges ON projects.id = pledges.project_id
-  ORDER BY projects.category
+  FROM projects 
   WHERE projects.category = 'music'
+  JOIN pledges ON projects.id = pledges.project_id
+  ORDER BY projects.category 
+  
   
   "
 end
